f18dc71f96310872a83dab2221ed472928c77b02
Tim Untersberger <timuntersberger2@gmail.com>
2020-02-08 19:49:58 +0100

    Update environment variables

--- src/orm-config.ts
+++ src/orm-config.ts
@@ -3,12 +3,16 @@ import Privilege from "./entity/Privilege";
 import Role from "./entity/Role";
 import BaseEntity from "./entity/BaseEntity";
 
+const host = process.env.KEYCAPE_DB_HOST || "localhost";
+const port = process.env.KEYCAPE_DB_PORT || "5432";
 const dbName = process.env.KEYCAPE_DB_NAME || "db";
 const user = process.env.KEYCAPE_DB_USERNAME || "admin";
 const password = process.env.KEYCAPE_DB_PASSWORD || "admin";
 
 export default {
   entities: [Account, Role, Privilege, BaseEntity],
+  host,
+  port,
   user,
   password,
   dbName,
