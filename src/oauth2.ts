ffd321f1d83c41d858f79ad9c48fb7483cfb1ccd
Tim Untersberger <timuntersberger2@gmail.com>
2020-02-10 22:32:42 +0100

    Extract generic oauth2 provider function for generating a router.

--- src/oauth2.ts
+++ src/oauth2.ts
@@ -1,6 +1,13 @@
 import Account from "./entity/Account";
 import jwt from "jsonwebtoken";
 import Container from "typedi";
+import express from "express";
+import crypto from "crypto";
+import ky from "ky-universal";
+import oauth2 from "simple-oauth2";
+import OAuth2ConnectionRepository from "./repository/OAuth2ConnectionRepository";
+import AccountRepository from "./repository/AccountRepository";
+import OAuth2Connection from "./entity/OAuth2Connection";
 
 const jwtSecret = Container.get<string>("jwtSecret");
 
@@ -28,4 +35,151 @@ export function createRefreshToken(account: Account) {
     },
     jwtSecret
   );
+}
+
+const oauth2Secret = Container.get<string>("oauth2Secret");
+const domain = Container.get<string>("domain");
+const port = Container.get<number>("port");
+
+function createProviderId(id: any) {
+  return crypto
+    .createHmac("sha256", oauth2Secret)
+    .update(String(id))
+    .digest("base64");
+}
+
+async function fetchProfileData(
+  url: string,
+  tokenType: string,
+  accessToken: string
+) {
+  return ky
+    .get(url, {
+      headers: {
+        Authorization: `${tokenType} ${accessToken}`
+      }
+    })
+    .then(res => res.json())
+    .catch(x => {
+      console.log(x);
+    });
+}
+
+export function createOAuth2ProviderRoute(
+  provider: "google" | "github",
+  profileDataUrl: string,
+  tokenHostUrl: string,
+  tokenPathUrl: string,
+  authorizePathUrl: string,
+  providerId: string,
+  providerSecret: string,
+  accessTokenType: string,
+  getUsernameFromData: (data: any) => string,
+  getEmailFromData: (data: any) => string,
+  scopes: string[]
+) {
+  const router = express.Router();
+  const oauth2Provider = oauth2.create({
+    client: {
+      id: providerId,
+      secret: providerSecret
+    },
+    auth: {
+      tokenHost: tokenHostUrl,
+      tokenPath: tokenPathUrl,
+      authorizePath: authorizePathUrl
+    }
+  });
+
+  router.get(`/oauth2/${provider}/signin`, async (_req, res) => {
+    const authorizationUrl = oauth2Provider.authorizationCode.authorizeURL({
+      redirect_uri: `http://${domain}:${port}/oauth2/${provider}/signin/callback`,
+      scope: scopes
+    });
+
+    res.redirect(authorizationUrl + "&access_type=offline&prompt=consent");
+  });
+
+  router.get(`/oauth2/${provider}/signin/callback`, async (req, res) => {
+    const connRepo = Container.get(OAuth2ConnectionRepository);
+
+    const { code } = req.query;
+    try {
+      const result = await oauth2Provider.authorizationCode.getToken({
+        code,
+        redirect_uri: `http://${domain}:${port}/oauth2/${provider}/signin/callback`
+      });
+
+      const token = oauth2Provider.accessToken.create(result);
+      const data = await fetchProfileData(
+        profileDataUrl,
+        accessTokenType,
+        token.token.access_token
+      );
+      const providerId = createProviderId(data.id);
+      const conn = await connRepo.findOneByProviderId(providerId);
+
+      res.cookie("refresh_token", createRefreshToken(conn.account));
+      res.redirect("http://localhost:8080/account");
+    } catch (err) {
+      console.error(`Access Token Error ${err.message}`);
+      console.log(err);
+      return res.status(500).json("Authenication failed");
+    }
+  });
+
+  router.get(`/oauth2/${provider}/signup`, async (_req, res) => {
+    const authorizationUrl = oauth2Provider.authorizationCode.authorizeURL({
+      redirect_uri: `http://${Container.get("domain")}:${Container.get(
+        "port"
+      )}/oauth2/${provider}/signup/callback`,
+      scope: scopes
+    });
+
+    res.redirect(authorizationUrl + "&access_type=offline&prompt=consent");
+  });
+
+  router.get(`/oauth2/${provider}/signup/callback`, async (req, res) => {
+    const accountRepo = Container.get(AccountRepository);
+    const connRepo = Container.get(OAuth2ConnectionRepository);
+    const { code } = req.query;
+    try {
+      const result = await oauth2Provider.authorizationCode.getToken({
+        code,
+        redirect_uri: `http://${domain}:${port}/oauth2/${provider}/signup/callback`
+      });
+
+      const token = oauth2Provider.accessToken.create(result);
+      const data = await fetchProfileData(
+        profileDataUrl,
+        accessTokenType,
+        token.token.access_token
+      );
+
+      const account = accountRepo.create({
+        username: getUsernameFromData(data),
+        email: getEmailFromData(data),
+        password: createProviderId(data.id)
+      });
+
+      const conn = new OAuth2Connection(
+        account.password,
+        token.token.refresh_token,
+        token.token.access_token,
+        "github"
+      );
+
+      conn.account = account;
+
+      await connRepo.persistAndFlush(conn);
+
+      res.redirect("http://localhost:8080/account");
+    } catch (err) {
+      console.error(`Access Token Error ${err.message}`);
+      console.log(err);
+      return res.status(500).json("Authenication failed");
+    }
+  });
+
+  return router;
 }
--- src/route/oauth2-github.ts
+++ src/route/oauth2-github.ts
@@ -1,135 +1,17 @@
-import express from "express";
-import oauth2 from "simple-oauth2";
-import Container from "typedi";
-import OAuth2ConnectionRepository from "../repository/OAuth2ConnectionRepository";
-import OAuth2Connection from "../entity/OAuth2Connection";
-import ky from "ky-universal";
-import crypto from "crypto";
-import AccountRepository from "../repository/AccountRepository";
-import { createRefreshToken } from "../oauth2";
+import { createOAuth2ProviderRoute } from "../oauth2";
 
-const router = express.Router();
-const domain = Container.get<string>("domain");
-const port = Container.get<number>("port");
-const oauth2Secret = Container.get<string>("oauth2Secret");
-
-function createGithubId(id: any) {
-  return crypto
-    .createHmac("sha256", oauth2Secret)
-    .update(String(id))
-    .digest("base64");
-}
-
-async function fetchProfileData(accessToken: string) {
-  return ky
-    .get("https://api.github.com/user", {
-      headers: {
-        Authorization: `token ${accessToken}`
-      }
-    })
-    .then(res => res.json())
-    .catch(x => {
-      console.log(x);
-    });
-}
-
-const oauth2Github = oauth2.create({
-  client: {
-    id: "d42895f0b5c7fce9b677",
-    secret: "c4262d3ed5480f7e1ff601eb4244e409ab367ef7"
-  },
-  auth: {
-    tokenHost: "https://github.com/",
-    tokenPath: "https://github.com/login/oauth/access_token",
-    authorizePath: "https://github.com/login/oauth/authorize"
-  }
-});
-
-router.get("/oauth2/github/signin", async (_req, res) => {
-  const authorizationUrl = oauth2Github.authorizationCode.authorizeURL({
-    redirect_uri: `http://${domain}:${port}/oauth2/github/signin/callback`,
-    scope: ["read:user"]
-  });
-
-  res.redirect(authorizationUrl + "&access_type=offline&prompt=consent");
-});
-
-router.get("/oauth2/github/signin/callback", async (req, res) => {
-  const connRepo = Container.get(OAuth2ConnectionRepository);
-
-  const { code } = req.query;
-  try {
-    const result = await oauth2Github.authorizationCode.getToken({
-      code,
-      redirect_uri: `http://${domain}:${port}/oauth2/github/signin/callback`
-    });
-
-    const token = oauth2Github.accessToken.create(result);
-
-    const data = await fetchProfileData(token.token.access_token);
-
-    const githubId = createGithubId(data.id);
-
-    const conn = await connRepo.findOneByProviderId(githubId);
-
-    res.cookie("refresh_token", createRefreshToken(conn.account));
-    res.redirect("http://localhost:8080/account");
-  } catch (err) {
-    console.error(`Access Token Error ${err.message}`);
-    console.log(err);
-    return res.status(500).json("Authenication failed");
-  }
-});
-
-router.get("/oauth2/github/signup", async (_req, res) => {
-  const authorizationUrl = oauth2Github.authorizationCode.authorizeURL({
-    redirect_uri: `http://${Container.get("domain")}:${Container.get(
-      "port"
-    )}/oauth2/github/signup/callback`,
-    scope: ["read:user"]
-  });
-
-  res.redirect(authorizationUrl + "&access_type=offline&prompt=consent");
-});
-
-router.get("/oauth2/github/signup/callback", async (req, res) => {
-  const accountRepo = Container.get(AccountRepository);
-  const connRepo = Container.get(OAuth2ConnectionRepository);
-  const { code } = req.query;
-  try {
-    const result = await oauth2Github.authorizationCode.getToken({
-      code,
-      redirect_uri: `http://${domain}:${port}/oauth2/github/signup/callback`
-    });
-
-    const token = oauth2Github.accessToken.create(result);
-    const data = await fetchProfileData(token.token.access_token);
-
-    const account = accountRepo.create({
-      username: data.login,
-      email: data.email,
-      password: createGithubId(data.id)
-    });
-
-    console.log(token.token);
-
-    const conn = new OAuth2Connection(
-      account.password,
-      token.token.refresh_token,
-      token.token.access_token,
-      "github"
-    );
-
-    conn.account = account;
-
-    await connRepo.persistAndFlush(conn);
-
-    res.redirect("http://localhost:8080/account");
-  } catch (err) {
-    console.error(`Access Token Error ${err.message}`);
-    console.log(err);
-    return res.status(500).json("Authenication failed");
-  }
-});
+const router = createOAuth2ProviderRoute(
+  "github",
+  "https://api.github.com/user",
+  "https://github.com",
+  "https://github.com/login/oauth/access_token",
+  "https://github.com/login/oauth/authorize",
+  "d42895f0b5c7fce9b677",
+  "c4262d3ed5480f7e1ff601eb4244e409ab367ef7",
+  "token",
+  data => data.login,
+  data => data.email,
+  ["read:user"]
+);
 
 export default router;
